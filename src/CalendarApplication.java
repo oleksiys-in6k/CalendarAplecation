fd0a6b2178460e14981c575be376af70b94f93c6
oleksiys-in6k <oleksiys@interlink-ua.com>
2015-05-27 10:30:59 +0300

    remove static from CalendarApplication class

--- src/CalendarApplication.java
+++ src/CalendarApplication.java
@@ -1,36 +1,42 @@
 
-import java.io.IOException;
+import java.io.FileNotFoundException;
+
 import java.util.*;
 
 class CalendarApplication {
 
+    public void writeEachMonthInOwnFile(List<MonthCalendarFile> allMonths) throws FileNotFoundException {
+        ListIterator<MonthCalendarFile> it = allMonths.listIterator();
+        while(it.hasNext())
 
-    public static void main(String args[]) throws IOException {
-
-        InputConfigReader config = new InputConfigReader();
-        List<Year> years = config.getYearsFromConfig();
-
-        List<MonthCalendarFile> list = new ArrayList<>();
-
-        for (Year year : years) {
-            list.addAll(year.getMonths());
-        }
-
-        ListIterator<MonthCalendarFile> it =  list.listIterator();
-        while (it.hasNext()) {
+        {
             MonthCalendarFile previous = null, next = null;
 
             if (it.hasPrevious()) {
-                previous = list.get(it.previousIndex());
+                previous = allMonths.get(it.previousIndex());
             }
 
             MonthCalendarFile calendarFile = it.next();
 
             if (it.hasNext()) {
-                next = list.get(it.nextIndex());
+                next = allMonths.get(it.nextIndex());
             }
 
             calendarFile.writeToFile(previous, next);
         }
-   }
+    }
+
+    public List<MonthCalendarFile> creatingListOfAllMonths() throws FileNotFoundException {
+        InputConfigReader config = new InputConfigReader();
+        List<Year> years = config.getYearsFromConfig();
+
+        List<MonthCalendarFile> allMonths = new ArrayList<>();
+
+        for (Year year : years) {
+            allMonths.addAll(year.getMonths());
+        }
+        return allMonths;
+    }
+
+
 }
